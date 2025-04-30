2e7e832cd505991bb0bb99680e9fe09d884f8c6f
Alex Chee <xxchee555xx@gmail.com>
2025-05-01 00:23:28 +0800

    🧹 CLEAN : remove music and unusable features

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-initial_extensions = ["cogs.basic", "cogs.music", "cogs.web"]
+initial_extensions = ["cogs.basic", "cogs.web"]
 
 # Initialize variables
 try:
